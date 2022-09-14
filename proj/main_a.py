1998e32d558f2370dc01588afc389590d03b992d
AntonioDRSousa <53109191+AntonioDRSousa@users.noreply.github.com>
2022-09-13 22:09:45 -0300

    Add files via upload

---
+++ proj/main_a.py
@@ -0,0 +1,38 @@
+import sys, os
+import time
+import p0
+import p1
+import p2
+import script
+
+sys.path.append(os.environ['PYDFHOME'])
+from pyDF import *
+
+nprocs = int(sys.argv[1])
+stride = float(sys.argv[2])
+
+graph = DFGraph()
+sched = Scheduler(graph, nprocs, mpi_enabled = False)
+
+R = Node(sum_total, nprocs)
+graph.add(R)
+
+SP = Node(script.main, 0)
+P0 = Node(p0.processo_p0, 1)
+P1 = Node(p1.processo_p1, 1)
+P2 = Node(p2.processo_p2, 1)
+
+graph.add(SP)
+graph.add(P0)
+graph.add(P1)
+graph.add(P2)
+
+P0.add_edge(SP, 0)
+P1.add_edge(P0, 0)
+P2.add_edge(P1, 0)
+
+t0 = time.time()
+sched.start()
+t1 = time.time()
+
+print("Execution time "+str(t1-t0))
